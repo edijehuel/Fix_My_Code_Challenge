e12f0d51bcbdc86f015d5339381526ce9cf1094b
edijehuel <jehuelassaledev99@gmail.com>
2022-08-21 20:45:00 -0700

    Hot Fix

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -76,15 +76,12 @@ if __name__ == '__main__':
     if user_2.password is not None:        
         print("User.password should be None if setter to an integer")               
     if not user_1.is_valid_password(u_pwd):        
-        print("is_valid_password should return True if it's the right \
-password")               
+        print("is_valid_password should return True if it's the right \password")               
     if user_1.is_valid_password("Fakepwd"):        
-        print("is_valid_password should return False if it's not the right \
-password")
+        print("is_valid_password should return False if it's not the right \password")
     if user_1.is_valid_password(None):        
         print("is_valid_password should return False if compare with None")                
     if user_1.is_valid_password(89):        
         print("is_valid_password should return False if compare with integer")              
     if user_2.is_valid_password("No pwd"):        
-        print("is_valid_password should return False if no password set \
-before")        
+        print("is_valid_password should return False if no password set \before")        
